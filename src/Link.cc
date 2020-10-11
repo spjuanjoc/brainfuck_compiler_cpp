89361b6d1bf7d9cdf1b0f8372a4269a84e5b9f37
Juan Jose Castellanos <juan.castellanos@smartmatic.com>
2020-10-10 21:19:51 -0500

    add system call abstraction

--- src/Link.cc
+++ src/Link.cc
@@ -2,9 +2,12 @@
 // Created by juan.castellanos on 17/09/20.
 //
 
-#include <cstdlib>
+#include "Exec.h"
+#include <iostream>
 
 void link()
 {
-  std::system("ld -lc tmp.o -o hello -I /lib64/ld-linux-x86-64.so.2");
+  std::string command{"ld -lc tmp.o -o hello -I /lib64/ld-linux-x86-64.so.2"};
+  std::string result = execute(command);
+  std::cout << result << '\n';
 }
